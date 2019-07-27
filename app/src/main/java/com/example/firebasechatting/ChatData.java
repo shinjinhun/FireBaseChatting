caf94032a2d9afaf4fa60ebb13584ef57ff8891b
shinjinhun <mamsorea@gmail.com>
2019-07-27 12:52:07 +0900

    FirebaseDatabaseChatting

---
+++ app/src/main/java/com/example/firebasechatting/ChatData.java
@@ -0,0 +1,28 @@
+package com.example.firebasechatting;
+
+import java.io.Serializable;
+
+/**
+ * Created by KPlo on 2018. 11. 3..
+ */
+
+public class ChatData {
+    private String msg;
+    private String nickname;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
